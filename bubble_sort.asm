8be4f21ce31df80815c0abfd0348a5dd17038b1c
Ana de Paula <anaspaula17@gmail.com>
2025-12-04 00:53:42 -0300

    exibindo informações do array e tempo de ordenação, em vez de timestamps

--- bubble_sort.asm
+++ bubble_sort.asm
@@ -9,12 +9,12 @@
 # Vetores de teste (não ordenados)
 ############################################################
 
+array_name: .asciiz "array n=200, sortedness=5"
 array: .word 524, 872, 560, 28, 883, 149, 945, 17, 984, 931, 872, 974, 5, 160, 21, 12, 958, 511, 616, 331, 897, 702, 766, 904, 33, 626, 802, 143, 19, 781, 508, 68, 367, 708, 252, 456, 587, 35, 98, 569, 255, 228, 30, 869, 758, 622, 908, 892, 784, 679, 296, 417, 641, 753, 298, 935, 887, 556, 76, 472, 622, 795, 422, 895, 803, 415, 982, 162, 506, 920, 385, 327, 795, 213, 382, 345, 900, 523, 255, 53, 709, 758, 612, 866, 112, 376, 433, 790, 522, 815, 104, 559, 439, 201, 436, 451, 15, 960, 185, 456, 380, 28, 504, 107, 397, 556, 401, 473, 741, 748, 266, 338, 893, 130, 996, 937, 314, 925, 130, 521, 808, 143, 516, 336, 845, 986, 659, 335, 333, 719, 441, 726, 896, 147, 564, 143, 456, 502, 272, 70, 366, 637, 765, 417, 395, 691, 434, 8, 502, 429, 23, 310, 444, 389, 624, 507, 798, 749, 975, 446, 415, 921, 317, 256, 829, 49, 890, 694, 133, 489, 820, 431, 342, 80, 821, 671, 69, 912, 475, 249, 447, 130, 801, 74, 589, 664, 220, 765, 463, 323, 532, 329, 755, 543, 122, 233, 759, 314, 134, 763 # n=200, sortedness=5
-array_length: 100
-msg_a1_before: .asciiz "Array antes: "
-msg_a1_after:  .asciiz "Array depois: "
-msg_timestamp_before: .asciiz "Timestamp inicio: "
-msg_timestamp_after: .asciiz "Timestamp fim: "
+array_length: 10
+msg_before: .asciiz "before: "
+msg_after:  .asciiz "after: "
+msg_elapsed_ms: .asciiz "milisseconds elapsed: "
 semicolon: .asciiz ":"
 newline:       .asciiz "\n"
 space:         .asciiz " "
@@ -30,9 +30,16 @@ main:
     ########################################################
     # array
     ########################################################
+    # imprime o nome do array
+    li   $v0, 4
+    la   $a0, array_name
+    syscall
+    la $a0, newline
+    syscall
+
     # imprime "array antes"
     li   $v0, 4
-    la   $a0, msg_a1_before
+    la   $a0, msg_before
     syscall
 
     # imprime conteúdo de array antes de ordenar
@@ -40,18 +47,24 @@ main:
     lw   $a1, array_length
     jal  print_array
 
-    jal print_timestamp_inicio
+    # captura a timestamp atual e armazena em $s5
+    li  $v0, 30
+    syscall
+    la $s5, ($a0)
 
     # chama bubble_sort(array, 5)
     la   $a0, array
     lw   $a1, array_length
     jal  bubble_sort
 
-    jal print_timestamp_fim
+    # captura a timestamp atual e armazena em $s6
+    li  $v0, 30
+    syscall
+    la $s6, ($a0)
     
     # imprime "array depois"
     li   $v0, 4
-    la   $a0, msg_a1_after
+    la   $a0, msg_after
     syscall
 
     # imprime conteúdo de array depois de ordenar
@@ -59,61 +72,24 @@ main:
     lw   $a1, array_length
     jal  print_array
 
+    # calcula tempo de duração
+    sub $s5, $s6, $s5
+    # imprime "elapsed time"
+    li   $v0, 4
+    la   $a0, msg_elapsed_ms
+    syscall
+    li   $v0, 1
+    la   $a0, ($s5)
+    syscall
+    li  $v0, 4
+    la $a0, newline
+    syscall
+
     ########################################################
     # Encerrar o programa
     ########################################################
     li   $v0, 10          # syscall 10 = exit
     syscall
-
-print_timestamp_inicio:
-    li  $v0, 4
-    la $a0, msg_timestamp_before
-    syscall
-        
-    li  $v0, 30
-    syscall
-
-    li $v0 1
-    syscall
-    
-    li  $v0, 4
-    la $a0, semicolon
-    syscall
-    
-    li $v0 1
-    la $a0, ($a1)
-    syscall
-    
-    li  $v0, 4
-    la $a0, newline
-    syscall
-
-    jr   $ra
-    
-print_timestamp_fim:
-    li  $v0, 4
-    la $a0, msg_timestamp_after
-    syscall
-        
-    li  $v0, 30
-    syscall
-
-    li $v0 1
-    syscall
-    
-    li  $v0, 4
-    la $a0, semicolon
-    syscall
-    
-    li $v0 1
-    la $a0, ($a1)
-    syscall
-    
-    li  $v0, 4
-    la $a0, newline
-    syscall
-
-    jr   $ra
 
 ############################################################
 # print_array
